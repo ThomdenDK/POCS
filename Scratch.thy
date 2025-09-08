0733dd26e9cf6bf91a944b65bcabdb242bfb3918
ThomdenDK <thomdendk@gmail.com>
2025-09-08 21:18:18 +0200

    more work

--- Scratch.thy
+++ Scratch.thy
@@ -1,5 +1,5 @@
 theory Scratch
-  imports Main
+  imports Main "HOL.Rings"
 
 begin
 
@@ -7,11 +7,25 @@ class ssr = order +
   fixes oplus :: "'a \<Rightarrow> 'a \<Rightarrow> 'a"  (infixl "\<oplus>" 65)
   fixes otimes :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" (infixl "\<otimes>" 70)
   fixes one :: "'a"
+  assume assoc
 
 (*Since it is impossible to quantify over type constructors in Isabelle, 
 we cannot make a generic set type in the GraphOf type definition, and must make a class*)
-type_synonym ('a, 's) Weighted = "('s \<times> 'a) set"
+type_synonym ('a, 's) Weighted = "('s \<times> 'a) list"
+
+definition scaleWeighted :: "'s::ssr \<Rightarrow> ('a, 's) Weighted \<Rightarrow> ('a, 's) Weighted" where
+  "scaleWeighted p = map (\<lambda>(els, ela). (els \<oplus> p, ela))"
+
+definition returnWeighted :: "'a \<Rightarrow> ('a, 's::ssr) Weighted " where
+  "returnWeighted x = [(one, x)]"
+
+fun bindWeighted :: "('a, 's::ssr) Weighted \<Rightarrow> ('a \<Rightarrow> ('a, 's) Weighted) \<Rightarrow> ('a, 's) Weighted" where
+  "bindWeighted [] k = []" |
+  "bindWeighted ((p, x) # xs) k = (scaleWeighted p (k x)) @ (bindWeighted xs k)"
+
 type_synonym ('a, 's) GraphOfWeighted = "'a \<Rightarrow> ('a, 's) Weighted"
+(*typedef ('a, 's) GraphOfWeighted = "UNIV :: ('a \<Rightarrow> ('a, 's) Weighted) set"
+  by auto*)
 
 (*The ssr option is not enforced anywhere, and this enforcement is not possible in a
 type synonym. Consider switching to a definition.*)
@@ -69,16 +83,30 @@ fun connect :: "('a, 's::ssr) GraphOf \<Rightarrow> ('a, 's) GraphOf \<Rightarro
   "connect f g = (
     relational_product (wset_as_set f) (wset_as_set g)
   )"
-
+(*
 fun isSmallest :: "('s::ssr \<times> 'a) \<Rightarrow> ('a, 's) Weighted \<Rightarrow> bool" where
   "isSmallest (a, b) w = fold (\<lambda>el. \<lambda>acc. acc \<or> (let (p, q) = el in if p \<le> a \<and> b = q then false else true)) w true"
 
 fun filterWeighted :: "('a, 's::ssr) Weighted \<Rightarrow> ('a, 's) Weighted" where
   "filterWeighted w = fold (\<lambda>el. \<lambda>acc. if isSmallest el w then (insert el acc) else acc) emptySet w"
-
+*)
 fun connectWeighted :: "('a, 's::ssr) GraphOfWeighted \<Rightarrow> ('a , 's) GraphOfWeighted \<Rightarrow> ('a, 's) GraphOfWeighted" where
-  "connectWeighted f g x = {(v \<oplus> w, z) | y v z w. (v, y) \<in> f x \<and> (w, z) \<in> g y}"
+  "connectWeighted f g x = 
+    fold (\<lambda>(els, ela) acc. (map (\<lambda>(gs, ga). (gs \<oplus> els, ga)) (g ela)) @ acc) (f x) []"
 
+fun graphUnionWeighted :: "('a, 's::ssr) GraphOfWeighted \<Rightarrow> ('a , 's) GraphOfWeighted \<Rightarrow> ('a, 's) GraphOfWeighted" where
+  "graphUnionWeighted f g x = f x @ g x"
+
+fun naiveStar :: "('a, 's::ssr) GraphOfWeighted \<Rightarrow> ('a, 's) GraphOfWeighted" where
+  "naiveStar g = graphUnionWeighted returnWeighted (connectWeighted (naiveStar g) g)"
+
+fun exp :: "('a, 's::ssr) GraphOfWeighted \<Rightarrow> nat \<Rightarrow> ('a, 's) GraphOfWeighted" where
+  "exp g 0 = returnWeighted" |
+  "exp g (Suc n) = connectWeighted g (exp g n)"
+
+(*instantiation Weighted :: "Monad"
+begin
+end*)
 
 codatatype ('s, 'a) heap = "Heap 'a (('s \<times> ('s, 'a) heap) list)"
 
